32690be855c20c4589219988d7a9f797b63c7bc5
luisrayas2000 <luisrayas2000@gmail.com>
2022-07-07 15:10:59 -0500

    initial commit.

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+project_slug = "{{ cookiecutter.project_slug }}"
+
+ERROR_COLOR = "\x1b[31m"
+MESSAGE_COLOR = "\x1b[34m"
+RESET_ALL = "\x1b[0m"
+
+if project_slug.startswith("x"):
+   print(f"{ERROR_COLOR}ERROR: {project_slug=} is not a valid name for this template.{RESET_ALL}")
+
+   sys.exit(1)
+
+print(f"{MESSAGE_COLOR}Let's do it! You're are going to create something awesome!")
+print(f"Creating project at { os.getcwd() }{RESET_ALL}")
+
+import os
+import subprocess
+
+MESSAGE_COLOR = "\x1b[34m"
+RESET_ALL = "\x1b[0m"
+
+print(f"{MESSAGE_COLOR}Almost done!")
+print(f"Initializing a git repository...{RESET_ALL}")
+
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+
+print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
---
+++ {{ cookiecutter.project_slug}}/README.md
@@ -0,0 +1,8 @@
+# {{ cookiecutter.project_title }} 
+
+By: {{ cookiecutter.project_author_name }}.
+
+Version: {{ cookiecutter.python_version }}
+
+{{ cookiecutter.project_description }}
+
